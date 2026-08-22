21b14fb50520e9e341dcff51e5228b7ba60a8540
WRNC.app <swearlikeasailorllc@gmail.com>
2026-08-22 13:30:28 -0400

    fix: keep build passport internally scrollable on web

--- app/(app)/vehicle/[id]/passport.tsx
+++ app/(app)/vehicle/[id]/passport.tsx
@@ -44,7 +44,11 @@ export default function VehiclePassportRoute() {
 
   return (
     <SafeAreaView className="flex-1 bg-wrnc-background">
-      <ScrollView className="flex-1 p-4" contentContainerStyle={{ paddingBottom: 24 }}>
+      <ScrollView
+        style={{ flex: 1 }}
+        contentContainerStyle={{ padding: 16, paddingBottom: 40 }}
+        showsVerticalScrollIndicator={false}
+      >
         <BuildPassportHeader
           vehicleTitle={vehicleSummary.title}
           vehicleSubtitle={vehicleSummary.subtitle}
